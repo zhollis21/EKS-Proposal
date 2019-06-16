f8c69e55d74572b1d96e665bbb334a2d190a0660
Zach Hollis <zach.hollis@dpra.com>
2019-06-16 14:36:37 -0400

    Making Progress on dynamic changing of the mud line and water line.
    Added the EKS blender file to the repo (the blend1 file is the last saved version as a way to recover changes)
    
    Updated the blender object so all the rods are unique and added inner corner points.
    
    Updated the waste material and temporarily are calling it oil.mat.
    
    Updated our prefabs to be mostly correct and deleted the old ones.
    
    Added scripts for simple camera control and filling the whole trapezoid with the oil (more to come on this later).
    
    Updated the project version

---
+++ Assets/Scripts/OilController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OilController : MonoBehaviour
+{
+    public Transform UpperFrontRight;
+    public Transform UpperFrontLeft;
+    public Transform UpperBackRight;
+    public Transform UpperBackLeft;
+    public Transform LowerFrontRight;
+    public Transform LowerFrontLeft;
+    public Transform LowerBackRight;
+    public Transform LowerBackLeft;
+
+    private Mesh mf;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mf = GetComponent<MeshFilter>().mesh;
+
+        mf.vertices = GetCorrectVertices();        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private Vector3[] GetCorrectVertices()
+    {
+        return new Vector3[]
+        {
+            LowerFrontRight.position,
+            LowerFrontLeft.position,
+            UpperFrontRight.position,
+            UpperFrontLeft.position,
+            UpperBackRight.position,
+            UpperBackLeft.position,
+            LowerBackRight.position,
+            LowerBackLeft.position,
+            UpperFrontRight.position,
+            UpperFrontLeft.position,
+            UpperBackRight.position,
+            UpperBackLeft.position,
+            LowerBackRight.position,
+            LowerFrontRight.position,
+            LowerFrontLeft.position,
+            LowerBackLeft.position,
+            LowerFrontLeft.position,
+            UpperFrontLeft.position,
+            UpperBackLeft.position,
+            LowerBackLeft.position,
+            LowerBackRight.position,
+            UpperBackRight.position,
+            UpperFrontRight.position,
+            LowerFrontRight.position
+        };
+    }
+}
