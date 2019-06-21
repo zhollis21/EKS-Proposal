aa5bea6265043c19f4f62cd85f05fca81c35b181
Zach Hollis <zach.hollis@dpra.com>
2019-06-20 23:26:17 -0400

    Huge Refactor Of The Model Controller (will rename some stuff like "oil" later)
    Added a variable for the top of the water.
    
    Condensed 24 variables into 6. :)
    
    Moved the slope and intercept calculations into methods
    
    Made the functions that get the vertices generic, so you give it a y and it tells you the coordinates.
    
    We now control both the top and bottom of both the water and oil.

--- Assets/Scripts/OilController.cs
+++ Assets/Scripts/OilController.cs
@@ -9,32 +9,16 @@ public class OilController : MonoBehaviour
     public MeshFilter WaterMeshFilter;
     public Transform OilWaterLine;
     public Transform UpperFrontRight;
-    public Transform UpperFrontLeft;
-    public Transform UpperBackRight;
-    public Transform UpperBackLeft;
     public Transform LowerFrontRight;
-    public Transform LowerFrontLeft;
-    public Transform LowerBackRight;
-    public Transform LowerBackLeft;
+    public float WaterTop;
+    public float OilBottom;
 
     private Mesh _oilMesh;
     private Mesh _waterMesh;
-    private float _xFrontRightSlope;
-    private float _xFrontRightIntercept;
-    private float _xFrontLeftSlope;
-    private float _xFrontLeftIntercept;
-    private float _xBackRightSlope;
-    private float _xBackRightIntercept;
-    private float _xBackLeftSlope;
-    private float _xBackLeftIntercept;
-    private float _zFrontRightSlope;
-    private float _zFrontRightIntercept;
-    private float _zFrontLeftSlope;
-    private float _zFrontLeftIntercept;
-    private float _zBackRightSlope;
-    private float _zBackRightIntercept;
-    private float _zBackLeftSlope;
-    private float _zBackLeftIntercept;
+    private float _xSlope;
+    private float _xIntercept;
+    private float _zSlope;
+    private float _zIntercept;
 
     // Start is called before the first frame update
     void Start()
@@ -42,7 +26,11 @@ public class OilController : MonoBehaviour
         _oilMesh = OilMeshFilter?.mesh;
         _waterMesh = WaterMeshFilter?.mesh;
 
-        CalculateSlopesAndIntercepts();
+        _xSlope = CalculateXOverYSlope(UpperFrontRight.position, LowerFrontRight.position);
+        _xIntercept = CalculateXIntercept(UpperFrontRight.position, _xSlope);
+
+        _zSlope = CalculateZOverYSlope(UpperFrontRight.position, LowerFrontRight.position);
+        _zIntercept = CalculateZIntercept(UpperFrontRight.position, _zSlope);
 
         _oilMesh.vertices = GetVerticesForOilMesh();
         _waterMesh.vertices = GetVerticesForWaterMesh();
@@ -57,101 +45,98 @@ public class OilController : MonoBehaviour
         _waterMesh.vertices = GetVerticesForWaterMesh();
     }
 
-    // The below equations are based on an inverted form of Slope-Intercept
-    private void CalculateSlopesAndIntercepts()
+    private float CalculateXOverYSlope(Vector3 P1, Vector3 P2)
     {
-        _xFrontRightSlope = (UpperFrontRight.position.x - LowerFrontRight.position.x) / (UpperFrontRight.position.y - LowerFrontRight.position.y);
-        _xFrontRightIntercept = UpperFrontRight.position.x - _xFrontRightSlope * UpperFrontRight.position.y;
-
-        _xFrontLeftSlope = (UpperFrontLeft.position.x - LowerFrontLeft.position.x) / (UpperFrontLeft.position.y - LowerFrontLeft.position.y);
-        _xFrontLeftIntercept = UpperFrontLeft.position.x - _xFrontLeftSlope * UpperFrontLeft.position.y;
-
-        _xBackRightSlope = (UpperBackRight.position.x - LowerBackRight.position.x) / (UpperBackRight.position.y - LowerBackRight.position.y);
-        _xBackRightIntercept = UpperBackRight.position.x - _xBackRightSlope * UpperBackRight.position.y;
-
-        _xBackLeftSlope = (UpperBackLeft.position.x - LowerBackLeft.position.x) / (UpperBackLeft.position.y - LowerBackLeft.position.y);
-        _xBackLeftIntercept = UpperBackLeft.position.x - _xBackLeftSlope * UpperBackLeft.position.y;
-
-        _zFrontRightSlope = (UpperFrontRight.position.z - LowerFrontRight.position.z) / (UpperFrontRight.position.y - LowerFrontRight.position.y);
-        _zFrontRightIntercept = UpperFrontRight.position.z - _zFrontRightSlope * UpperFrontRight.position.y;
-
-        _zFrontLeftSlope = (UpperFrontLeft.position.z - LowerFrontLeft.position.z) / (UpperFrontLeft.position.y - LowerFrontLeft.position.y);
-        _zFrontLeftIntercept = UpperFrontLeft.position.z - _zFrontLeftSlope * UpperFrontLeft.position.y;
-
-        _zBackRightSlope = (UpperBackRight.position.z - LowerBackRight.position.z) / (UpperBackRight.position.y - LowerBackRight.position.y);
-        _zBackRightIntercept = UpperBackRight.position.z - _zBackRightSlope * UpperBackRight.position.y;
-
-        _zBackLeftSlope = (UpperBackLeft.position.z - LowerBackLeft.position.z) / (UpperBackLeft.position.y - LowerBackLeft.position.y);
-        _zBackLeftIntercept = UpperBackLeft.position.z - _zBackLeftSlope * UpperBackLeft.position.y;
+        return (P2.x - P1.x) / (P2.y - P1.y);
     }
 
-    private Vector3 GetFrontRightAtOilWaterLine()
+    private float CalculateZOverYSlope(Vector3 P1, Vector3 P2)
     {
-        var point = new Vector3(0, OilWaterLine.position.y, 0);
+        return (P2.z - P1.z) / (P2.y - P1.y);
+    }
 
-        point.x = _xFrontRightSlope * OilWaterLine.position.y + _xFrontRightIntercept;
-        point.z = _zFrontRightSlope * OilWaterLine.position.y + _zFrontRightIntercept;
+    private float CalculateXIntercept(Vector3 P1, float xSlope)
+    {
+        return P1.x - xSlope * P1.y;
+    }
+
+    private float CalculateZIntercept(Vector3 P1, float zSlope)
+    {
+        return P1.z - zSlope * P1.y;
+    }
+
+    private Vector3 GetFrontRightVerticesAtY(float y)
+    {
+        var point = new Vector3(0, y, 0);
+
+        point.x = _xSlope * y + _xIntercept;
+        point.z = _zSlope * y + _zIntercept;
 
         return point;
     }
 
-    private Vector3 GetFrontLeftAtOilWaterLine()
+    private Vector3 GetFrontLeftVerticesAtY(float y)
     {
-        var point = new Vector3(0, OilWaterLine.position.y, 0);
+        var point = new Vector3(0, y, 0);
 
-        point.x = _xFrontLeftSlope * OilWaterLine.position.y + _xFrontLeftIntercept;
-        point.z = _zFrontLeftSlope * OilWaterLine.position.y + _zFrontLeftIntercept;
+        point.x = -(_xSlope * y + _xIntercept);
+        point.z = _zSlope * y + _zIntercept;
 
         return point;
     }
 
-    private Vector3 GetBackRightAtOilWaterLine()
+    private Vector3 GetBackRightVerticesAtY(float y)
     {
-        var point = new Vector3(0, OilWaterLine.position.y, 0);
+        var point = new Vector3(0, y, 0);
 
-        point.x = _xBackRightSlope * OilWaterLine.position.y + _xBackRightIntercept;
-        point.z = _zBackRightSlope * OilWaterLine.position.y + _zBackRightIntercept;
+        point.x = _xSlope * y + _xIntercept;
+        point.z = -(_zSlope * y + _zIntercept);
 
         return point;
     }
 
-    private Vector3 GetBackLeftAtOilWaterLine()
+    private Vector3 GetBackLeftVerticesAtY(float y)
     {
-        var point = new Vector3(0, OilWaterLine.position.y, 0);
+        var point = new Vector3(0, y, 0);
 
-        point.x = _xBackLeftSlope * OilWaterLine.position.y + _xBackLeftIntercept;
-        point.z = _zBackLeftSlope * OilWaterLine.position.y + _zBackLeftIntercept;
+        point.x = -(_xSlope * y + _xIntercept);
+        point.z = -(_zSlope * y + _zIntercept);
 
         return point;
     }
 
     private Vector3[] GetVerticesForWaterMesh()
     {
+        var WaterUpperFrontRight = GetFrontRightVerticesAtY(WaterTop);
+        var WaterUpperFrontLeft = GetFrontLeftVerticesAtY(WaterTop);
+        var WaterUpperBackRight = GetBackRightVerticesAtY(WaterTop);
+        var WaterUpperBackLeft = GetBackLeftVerticesAtY(WaterTop);
+
         // The Oil Water Line is the lower part of the water
-        var WaterLowerFrontRight = GetFrontRightAtOilWaterLine();
-        var WaterLowerFrontLeft = GetFrontLeftAtOilWaterLine();
-        var WaterLowerBackRight = GetBackRightAtOilWaterLine();
-        var WaterLowerBackLeft = GetBackLeftAtOilWaterLine();
+        var WaterLowerFrontRight = GetFrontRightVerticesAtY(OilWaterLine.position.y);
+        var WaterLowerFrontLeft = GetFrontLeftVerticesAtY(OilWaterLine.position.y);
+        var WaterLowerBackRight = GetBackRightVerticesAtY(OilWaterLine.position.y);
+        var WaterLowerBackLeft = GetBackLeftVerticesAtY(OilWaterLine.position.y);
 
         return new Vector3[]
         {
             // Front Face
             WaterLowerFrontRight,
             WaterLowerFrontLeft,
-            UpperFrontRight.position,
-            UpperFrontLeft.position,
+            WaterUpperFrontRight,
+            WaterUpperFrontLeft,
 
             // Back Face
-            UpperBackRight.position,
-            UpperBackLeft.position,
+            WaterUpperBackRight,
+            WaterUpperBackLeft,
             WaterLowerBackRight,
             WaterLowerBackLeft,
 
             // Top Face
-            UpperFrontRight.position,
-            UpperFrontLeft.position,
-            UpperBackRight.position,
-            UpperBackLeft.position,
+            WaterUpperFrontRight,
+            WaterUpperFrontLeft,
+            WaterUpperBackRight,
+            WaterUpperBackLeft,
 
             // Bottom Face
             WaterLowerBackRight,
@@ -161,39 +146,44 @@ public class OilController : MonoBehaviour
 
             // Left Face
             WaterLowerFrontLeft,
-            UpperFrontLeft.position,
-            UpperBackLeft.position,
+            WaterUpperFrontLeft,
+            WaterUpperBackLeft,
             WaterLowerBackLeft,
 
             // Right Face
             WaterLowerBackRight,
-            UpperBackRight.position,
-            UpperFrontRight.position,
+            WaterUpperBackRight,
+            WaterUpperFrontRight,
             WaterLowerFrontRight
         };
     }
 
     private Vector3[] GetVerticesForOilMesh()
     {
+        var OilLowerFrontRight = GetFrontRightVerticesAtY(OilBottom);
+        var OilLowerFrontLeft = GetFrontLeftVerticesAtY(OilBottom);
+        var OilLowerBackRight = GetBackRightVerticesAtY(OilBottom);
+        var OilLowerBackLeft = GetBackLeftVerticesAtY(OilBottom);
+
         // The Oil Water Line is the upper part of the oil 
-        var OilUpperFrontRight = GetFrontRightAtOilWaterLine();
-        var OilUpperFrontLeft = GetFrontLeftAtOilWaterLine();
-        var OilUpperBackRight = GetBackRightAtOilWaterLine();
-        var OilUpperBackLeft = GetBackLeftAtOilWaterLine();
+        var OilUpperFrontRight = GetFrontRightVerticesAtY(OilWaterLine.position.y);
+        var OilUpperFrontLeft = GetFrontLeftVerticesAtY(OilWaterLine.position.y);
+        var OilUpperBackRight = GetBackRightVerticesAtY(OilWaterLine.position.y);
+        var OilUpperBackLeft = GetBackLeftVerticesAtY(OilWaterLine.position.y);
 
         return new Vector3[]
         {
             // Front Face
-            LowerFrontRight.position,
-            LowerFrontLeft.position,
+            OilLowerFrontRight,
+            OilLowerFrontLeft,
             OilUpperFrontRight,
             OilUpperFrontLeft,
 
             // Back Face
             OilUpperBackRight,
             OilUpperBackLeft,
-            LowerBackRight.position,
-            LowerBackLeft.position,
+            OilLowerBackRight,
+            OilLowerBackLeft,
 
             // Top Face
             OilUpperFrontRight,
@@ -202,22 +192,22 @@ public class OilController : MonoBehaviour
             OilUpperBackLeft,
 
             // Bottom Face
-            LowerBackRight.position,
-            LowerFrontRight.position,
-            LowerFrontLeft.position,
-            LowerBackLeft.position,
+            OilLowerBackRight,
+            OilLowerFrontRight,
+            OilLowerFrontLeft,
+            OilLowerBackLeft,
 
             // Left Face
-            LowerFrontLeft.position,
+            OilLowerFrontLeft,
             OilUpperFrontLeft,
             OilUpperBackLeft,
-            LowerBackLeft.position,
+            OilLowerBackLeft,
 
             // Right Face
-            LowerBackRight.position,
+            OilLowerBackRight,
             OilUpperBackRight,
             OilUpperFrontRight,
-            LowerFrontRight.position
+            OilLowerFrontRight
         };
     }
 }
